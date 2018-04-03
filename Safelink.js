a8f21e395cea2bce730db4548c3cd85b91afbb0d
DimasPray <38005804+DimasPray@users.noreply.github.com>
2018-04-03 17:50:04 +0700

    Update Safelink.js

--- Safelink.js
+++ Safelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://safelink-ilmu.blogspot.com/p/mobile-legends-bang-bang-developers.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://safelink-ilmu.blogspot.co.id/p/mobile-legends-bang-bang-developers.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
